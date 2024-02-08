2e5c280d378dfe8f2c861321b3968785ea9911b4
evilleda <estuardovilleda2001@gmail.com>
2024-02-07 19:02:09 -0600

    feat: create all files we need

---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const Error = () => {
+  return (
+    <>
+      <h1>Error Page</h1>
+      <Link to='/'>Home Page</Link>
+    </>
+  );
+};
+export default Error;
