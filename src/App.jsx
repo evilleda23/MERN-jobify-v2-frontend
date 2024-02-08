e819b72f6b970e04599959d3449b16d36b408870
evilleda <estuardovilleda2001@gmail.com>
2024-02-07 19:02:23 -0600

    feat: first routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,54 @@
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
+import './index.css';
+
+import {
+  DashboardLayout,
+  Error,
+  HomeLayout,
+  Landing,
+  Login,
+  Register,
+} from './pages';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path='/'
+      element={<HomeLayout />}
+      errorElement={<Error />}
+    >
+      <Route
+        index
+        element={<Landing />}
+      />
+
+      <Route
+        path='register'
+        element={<Register />}
+      />
+      <Route
+        path='login'
+        element={<Login />}
+      />
+
+      <Route
+        path='dashboard'
+        element={<DashboardLayout />}
+      />
+      <Route
+        path='*'
+        element={<Error />}
+      />
+    </Route>
+  )
+);
+
 const App = () => {
-  return <div>Jobify App</div>;
+  return <RouterProvider router={router} />;
 };
 export default App;
