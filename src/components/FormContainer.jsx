f294df737ccb841b059ad5b18d6fb17980f0f367
evilleda <estuardovilleda2001@gmail.com>
2024-02-07 21:10:52 -0600

    feat: add boostrap

---
+++ src/components/FormContainer.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const FormContainer = ({ children }) => {
+  return (
+    <Container>
+      <Row className='justify-content-md-center'>
+        <Col
+          xs={12}
+          md={6}
+        >
+          {children}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default FormContainer;
+
+FormContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,50 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { Button, Card, Col, Form, Row } from 'react-bootstrap';
+import { FormContainer, Logo } from '../components';
+
 const Register = () => {
+  const [name, setName] = useState('');
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to='/login'>Login Page</Link>
-    </div>
+    <Wrapper>
+      <Card className='py-5 '>
+        <FormContainer>
+          <Form className='form'>
+            <Logo />
+            <h4>Register</h4>
+            <Form.Group
+              controlId='name'
+              className='my-3'
+            >
+              <Form.Label className='form-label'>Name</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Enter Name'
+                className='form-input'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+            <Button
+              type='submit'
+              variant='primary'
+              className='btn-block my-3'
+
+              // disabled={isLoading}
+            >
+              Register
+            </Button>
+            <Row className='py-3'>
+              <Col>
+                Already have an account? <Link to={`/login`}>Login here!</Link>
+              </Col>
+            </Row>
+          </Form>
+        </FormContainer>
+      </Card>
+    </Wrapper>
   );
 };
 export default Register;
